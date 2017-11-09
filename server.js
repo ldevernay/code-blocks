48a6f4d7c650b3b3985284927380dc0bfd509bf9
ldevernay <boblehamster@yahoo.fr>
2017-11-09 10:51:24 +0100

    .gitignore

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(express.static(__dirname + '/public'));
 // Set port to env variable for Heroku, 3000 for dev
 const port = process.env.PORT || 3000;
 
+
 app.listen(port, function () {
   console.log(`code-blocks listening on port ${port}!`)
 })
